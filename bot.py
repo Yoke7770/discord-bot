e9613f5c24dd8d8499011c2761ae2a2d27514a33
Ye Naing Aung <bow4129@gmail.com>
2026-08-22 00:41:45 +0630

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-if not TOKEN:238183becfd966e9b5cd11284f0813eea1925479941655489ad2b44a153ca6d9
+if not TOKEN:
     print("Error: DISCORD_TOKEN environment variable not set.")
     sys.exit(1)
 
